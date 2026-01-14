0fe16fab3251e11dd147edb0532fb2bad8de538e
S.A.M <akhilfilms02@gmail.com>
2026-01-14 15:03:06 -0600

    test dummy breadcrumb

--- src/pages/edit-user/index.tsx
+++ src/pages/edit-user/index.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import { UsersLayoutContextType } from "../manage-users/layout";
+
 export default function EditUserPage() {
+  const { setBreadcrumbLabel } = useOutletContext<UsersLayoutContextType>();
+
+  useEffect(() => {
+    setBreadcrumbLabel("Dummy User Name");
+  }, []);
+
   return <div>EditUserPage</div>;
 }

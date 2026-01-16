7055cf9e6557489f463523d2cb89fa930ccf5661
S.A.M <akhilfilms02@gmail.com>
2026-01-16 03:01:24 -0600

    added basic fetch

--- src/pages/dashboard-libraries/add/index.tsx
+++ src/pages/dashboard-libraries/add/index.tsx
@@ -1,3 +1,35 @@
+import { useSetAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { dashboardLoadingAtom } from "../../../lib/atoms";
+import {
+  CountryInfo,
+  CultureDto,
+} from "@jellyfin/sdk/lib/generated-client/models";
+import { fetchCountries, fetchCultures } from "../../../actions/utils";
+
 export default function AddLibraryPage() {
+  const setDashboardLoading = useSetAtom(dashboardLoadingAtom);
+  const [cultures, setCultures] = useState<CultureDto[]>([]);
+  const [countries, setCountries] = useState<CountryInfo[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setDashboardLoading(true);
+      try {
+        const [culturesData, countriesData] = await Promise.all([
+          fetchCultures(),
+          fetchCountries(),
+        ]);
+        setCultures(culturesData);
+        setCountries(countriesData);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setDashboardLoading(false);
+      }
+    };
+    loadData();
+  }, [setDashboardLoading]);
+
   return <div>AddLibraryPage</div>;
 }

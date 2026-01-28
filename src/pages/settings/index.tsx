dd016b837f9787945e8b436965932690934659f5
S.A.M <akhilfilms02@gmail.com>
2026-01-28 01:36:37 -0600

    remove unwanted code

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../../components/ui/card";
 import { AuroraBackground } from "../../components/aurora-background";
 import { SearchBar } from "../../components/search-component";
-import { Settings2, LayoutDashboard } from "lucide-react";
-import type { JellyfinUserWithToken } from "../../types/jellyfin";
-import { Link } from "react-router-dom";
+import { Settings2 } from "lucide-react";
 import SeerrSection from "../../components/settings/seerr-section";
 import ProfileSection from "../../components/settings/profile-section";
 import ThemeSection from "../../components/settings/theme-section";
 import UserPreferenceSection from "../../components/settings/user-preference-section";
 
 export default function SettingsPage() {
-  const [user, setUser] = useState<JellyfinUserWithToken | null>(null);
-
   return (
     <div className="relative px-4 py-6 max-w-full overflow-hidden">
       <AuroraBackground />
@@ -39,29 +28,7 @@ export default function SettingsPage() {
         <div className="grid gap-6">
           <ProfileSection />
           <SeerrSection />
-
-          {user?.Policy?.IsAdministrator ? (
-            <Card className="bg-card/80 backdrop-blur ">
-              <CardHeader className="flex flex-wrap items-start justify-between gap-3">
-                <CardTitle className="flex items-center gap-2 font-poppins text-lg">
-                  <LayoutDashboard className="h-5 w-5" />
-                  Dashboard
-                </CardTitle>
-                <Link
-                  to={"/dashboard"}
-                  className="inline-flex items-center gap-1 rounded-full border border-border/60 px-3 py-1 text-xs font-semibold text-muted-foreground transition hover:text-foreground"
-                >
-                  Open Dashboard
-                </Link>
-                <CardDescription className="w-full">
-                  Manage your Jellyfin server and system settings.
-                </CardDescription>
-              </CardHeader>
-            </Card>
-          ) : null}
-
           <UserPreferenceSection />
-
           <ThemeSection />
         </div>
       </div>
